cb754fe2ad7e57c6543cd7531404d87601f7f2d6
YevheniiN281 <enesterenko.mba2024@london.edu>
2023-05-18 18:53:22 +0100

    Updating a README file using the Commit bar

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # ENesterenko-DSB23
-Data Science in Business Class
+Data Science in Business Class at LBS
+
+Note 20230518 - trying to recreate Session 2 (Github) notes
+
+Now trying to push to Github - only a modified file
---
+++ Script-session-2.R
@@ -0,0 +1,9 @@
+usethis::create_from_github(
+  "https://github.com/kostis-christodoulou/dsb2023.git",
+  
+
+blogdown::new_site(theme = "MarcusVirg/forty",
+                   sample = TRUE,
+                   theme_example = TRUE,
+                   empty_dirs = TRUE,
+                   to_yaml = TRUE)
