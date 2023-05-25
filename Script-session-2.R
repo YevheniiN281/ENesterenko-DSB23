6ca4f7775035fe9e002cd0456421311d7a5a6a34
YevheniiN281 <enesterenko.mba2024@london.edu>
2023-05-25 13:00:15 +0100

    Blank Homework 3 file

--- Script-session-2.R
+++ Script-session-2.R
@@ -1,9 +1,10 @@
-usethis::create_from_github(
-  "https://github.com/kostis-christodoulou/dsb2023.git",
+# usethis::create_from_github(
+#  "https://github.com/kostis-christodoulou/dsb2023.git",
   
 
-blogdown::new_site(theme = "MarcusVirg/forty",
-                   sample = TRUE,
-                   theme_example = TRUE,
-                   empty_dirs = TRUE,
-                   to_yaml = TRUE)
+# blogdown::new_site(theme = "MarcusVirg/forty",
+#                   sample = TRUE,
+#                   theme_example = TRUE,
+#                   empty_dirs = TRUE,
+#                   to_yaml = TRUE)
+  
